62e92ff0027e450505d8dce3b29984d5b51052b5
hirohumi shibata <hiro-shiba@outlook.com>
2025-09-28 17:11:19 +0900

    EnemyPatternBuilder

---
+++ Assets/Script/EnemyPatternBuilder.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+
+namespace Nananami {
+    public class EnemyPatternBuilder {
+        public static List<string> Build(string start, string end) 
+        {
+            return new List<string>();
+        }
+    }
+}
