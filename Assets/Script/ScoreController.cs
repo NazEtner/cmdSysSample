469dc4b60736cd6c1950a54974c157e7396f730b
NazEtner <113223856+NazEtner@users.noreply.github.com>
2025-10-02 05:06:23 +0900

    increase_rateをエディタで調整可能に

--- Assets/Script/ScoreController.cs
+++ Assets/Script/ScoreController.cs
@@ -1,11 +1,13 @@
 using Nananami.Actors;
 using Nananami.Commands;
 using Nananami.Helpers;
+using UnityEngine;
 
 namespace Nananami
 {
     public class ScoreController : Actor
     {
+        [SerializeField] private float m_increase_rate;
         protected override void m_update()
         {
             base.m_update();
@@ -17,8 +19,7 @@ namespace Nananami
             {
                 if (message == "ScoreAddtionIncrease")
                 {
-                    const float increaseRate = 1.7f;
-                    int increaseValue = (int)((float)CommandVariableHelper.GetVariable<int>(instance.globalScheduler, "gameScoreAddition") * (increaseRate - 1.0f));
+                    int increaseValue = (int)((float)CommandVariableHelper.GetVariable<int>(instance.globalScheduler, "gameScoreAddition") * (m_increase_rate - 1.0f));
                     instance.globalScheduler.EnqueueCommand(new AddVariable<int>("gameScoreAddition", increaseValue));
                 }
             }
